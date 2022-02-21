0acdffa36de029e417f080a239178522edba2a66
mortrevere <leodroide@hotmail.fr>
2022-02-21 22:46:22 +0100

    temp endpoint for keys

--- client/init.sh
+++ client/init.sh
@@ -1,5 +1,5 @@
 wg-quick down lafayette
 apt install -y wireguard
-ls /etc/wireguard/lafayette.conf || curl -H "Token: jUkqQsMYeFADK1s1O8gb3BMjF" localhost:8000/keys > /etc/wireguard/lafayette.conf
+ls /etc/wireguard/lafayette.conf || curl -H "Token: jUkqQsMYeFADK1s1O8gb3BMjF" http://164.132.48.50:8000/keys > /etc/wireguard/lafayette.conf
 chmod 777 /etc/wireguard/lafayette.conf
 wg-quick up lafayette
