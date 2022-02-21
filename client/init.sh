3e31e3dd8687a8b497a4989413f03e205c557e6e
mortrevere <leodroide@hotmail.fr>
2022-02-21 23:13:29 +0100

    fix wireguard install on client

--- client/init.sh
+++ client/init.sh
@@ -1,6 +1,6 @@
 apt update -y
 wg-quick down lafayette
-apt install -y wireguard
+apt install -y wireguard dkms raspberrypi-kernel-headers wireguard-dkms wireguard-tools
 ls /etc/wireguard/lafayette.conf || curl -H "Token: jUkqQsMYeFADK1s1O8gb3BMjF" http://164.132.48.50:8000/keys > /etc/wireguard/lafayette.conf
 chmod 777 /etc/wireguard/lafayette.conf
 wg-quick up lafayette
