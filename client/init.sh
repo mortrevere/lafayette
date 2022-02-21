9e14636dd10f1a212e5ef5c4432de9927840bd6c
mortrevere <leodroide@hotmail.fr>
2022-02-21 22:47:43 +0100

    update before install

--- client/init.sh
+++ client/init.sh
@@ -1,3 +1,4 @@
+apt update
 wg-quick down lafayette
 apt install -y wireguard
 ls /etc/wireguard/lafayette.conf || curl -H "Token: jUkqQsMYeFADK1s1O8gb3BMjF" http://164.132.48.50:8000/keys > /etc/wireguard/lafayette.conf
