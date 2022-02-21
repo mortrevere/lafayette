1ff969349e5c9cc61af1046fe428b3c118a67cb0
mortrevere <leodroide@hotmail.fr>
2022-02-22 00:14:58 +0100

    auto-provision grafana dashboard

--- run-grafana.sh
+++ run-grafana.sh
@@ -1,4 +1,5 @@
 docker run --rm --network host --env-file $(pwd)/grafana.env \
        -v $(pwd)/grafana-prom-datasource.yaml:/etc/grafana/provisioning/datasources/prometheus.yml \
        -v $(pwd)/grafana-dashboard.yaml:/etc/grafana/provisioning/dashboards/grafana-dashboard.yml \
+       -v $(pwd)/dashboard.json:/var/lib/grafana/dashboards/dashboard.json \
        --name grafana grafana/grafana-oss:8.2.0
