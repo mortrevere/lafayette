1573ecb1a0f9f3886dd4ce4191ab12eb11ce930c
mortrevere <leodroide@hotmail.fr>
2022-02-22 11:28:52 +0100

    persist prom data

--- run-prometheus.sh
+++ run-prometheus.sh
@@ -1,1 +1,3 @@
-docker run --rm -d --network host -v $(pwd)/prometheus.yml:/etc/prometheus/prometheus.yml prom/prometheus --config.file=/etc/prometheus/prometheus.yml --storage.tsdb.path=/prometheus --web.console.libraries=/usr/share/prometheus/console_libraries --web.console.templates=/usr/share/prometheus/consoles --web.external-url=/prometheus/
+ls /opt/lafayette/prometheus-data || mkdir /opt/lafayette/prometheus-data
+chown 65534:65534 /opt/lafayette/prometheus-data
+docker run --rm -d --network host -v /opt/lafayette/prometheus-data/:/prometheus -v $(pwd)/prometheus.yml:/etc/prometheus/prometheus.yml prom/prometheus --config.file=/etc/prometheus/prometheus.yml --storage.tsdb.path=/prometheus --web.console.libraries=/usr/share/prometheus/console_libraries --web.console.templates=/usr/share/prometheus/consoles --web.external-url=/prometheus/
