c1534624c0bb71034dff1e1f22a3d6c5c914e9c1
mortrevere <leodroide@hotmail.fr>
2022-02-22 11:50:01 +0100

    remove grafana password from repo

--- run-grafana.sh
+++ run-grafana.sh
@@ -1,4 +1,5 @@
 docker run --rm -d --network host --env-file $(pwd)/grafana.env \
+       -e GF_SECURITY_ADMIN_PASSWORD=$(cat /opt/lafayette/grafana.pw) \
        -v $(pwd)/grafana-prom-datasource.yaml:/etc/grafana/provisioning/datasources/prometheus.yml \
        -v $(pwd)/grafana-dashboard.yaml:/etc/grafana/provisioning/dashboards/grafana-dashboard.yml \
        -v $(pwd)/dashboard.json:/var/lib/grafana/dashboards/dashboard.json \
