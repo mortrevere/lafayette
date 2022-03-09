d0f121a2caa668914416401a1284a4a5a22e9320
mortrevere <leodroide@hotmail.fr>
2022-03-09 22:43:21 +0100

    allow to re-run installation

--- install-everything.sh
+++ install-everything.sh
@@ -8,7 +8,7 @@ sudo apt-get install -y \
     curl \
     gnupg \
     lsb-release
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+ls /usr/share/keyrings/docker-archive-keyring.gpg || (curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg)
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
