8f2f9fcc801e9312b84af85eaa17b94987ba34d3
mortrevere <leodroide@hotmail.fr>
2022-03-09 23:01:44 +0100

    fail

--- run-prometheus.sh
+++ run-prometheus.sh
@@ -1,3 +1,3 @@
 ls /opt/lafayette/prometheus-data || mkdir /opt/lafayette/prometheus-data
 chown 65534:65534 /opt/lafayette/prometheus-data
-docker run --name prometheus --rm -d --network host -v /opt/lafayette/prometheus-data/:/prometheus -v $(pwd)/prometheus.yml:/etc/prometheus/prometheus.yml prom/prometheus --config.file=/etc/prometheus/prometheus.yml --storage.tsdb.path=/prometheus:v2.33.5 --web.console.libraries=/usr/share/prometheus/console_libraries --web.console.templates=/usr/share/prometheus/consoles --web.external-url=/prometheus/
+docker run --name prometheus --rm -d --network host -v /opt/lafayette/prometheus-data/:/prometheus -v $(pwd)/prometheus.yml:/etc/prometheus/prometheus.yml prom/prometheus:v2.33.5 --config.file=/etc/prometheus/prometheus.yml --storage.tsdb.path=/prometheus --web.console.libraries=/usr/share/prometheus/console_libraries --web.console.templates=/usr/share/prometheus/consoles --web.external-url=/prometheus/
